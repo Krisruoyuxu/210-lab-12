50b25f77905ad7d1ab7dc7ce85ce32292ac7297c
dickxumamba <xuruoyu27@gmail.com>
2025-09-20 21:26:29 +0800

    updated, trying to read the data from the txt files and move them into the array

--- lab12.cpp
+++ lab12.cpp
@@ -9,9 +9,19 @@ const int top15_size = 15; // our array only stores the top15 athletes in the 10
 
 int main() {
 	
-    array<double, top15_size> top15_records{};
+    array<double, top15_size> top15_records{}; // we use this array to store the final top15 records
+    array<double, 50> all50_records{};
+
     ifstream fin("2000top50.txt");
+    double mark;
+    while (fin >> mark) {
+        // extract the mark from the file and place them into the all50 array
+        
+    }
     
+    for (double score: top15_records){
+        cout << score <<endl; // we can see the original 15 elements are all 0
+    }
 
 	return 0;
 }
