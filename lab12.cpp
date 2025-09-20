b8b6f08829b6e76a07708aa7ff6018f2c2f3873e
dickxumamba <xuruoyu27@gmail.com>
2025-09-20 21:14:56 +0800

    collected data, start our program

--- lab12.cpp
+++ lab12.cpp
@@ -2,13 +2,16 @@
 #include <algorithm>  // for sort(), find()
 #include <numeric>    // for accumulate()
 #include <array>
+#include <fstream>
 using namespace std;
 
 const int top15_size = 15; // our array only stores the top15 athletes in the 100m race
-//
+
 int main() {
 	
     array<double, top15_size> top15_records{};
+    ifstream fin("2000top50.txt");
+    
 
 	return 0;
 }
