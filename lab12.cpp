7c218ad1851a98619bfe5ae9d4d0774c20702b03
dickxumamba <xuruoyu27@gmail.com>
2025-09-20 20:27:10 +0800

    first commit, start the lab12, the array is used to store the world top15 records in 100m race.

---
+++ lab12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>  // for sort(), find()
+#include <numeric>    // for accumulate()
+#include <array>
+using namespace std;
+
+const int top15_size = 15; // our array only stores the top15 athletes in the 100m race
+
+int main() {
+	
+    array<double, top15_size> records{};
+
+	return 0;
+}
+
